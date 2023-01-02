255889458bfdd888deede57de66b95e4c0b88648
Vikram Singh <vs125609@gmail.com>
2023-01-02 07:57:26 +0530

    added mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const app = require('./app');
+const mongoose = require('mongoose');
 const port = 3000 || process.env.PORT;
 app.listen(port, () => {
     console.log(`Server is running at ${port}`)
