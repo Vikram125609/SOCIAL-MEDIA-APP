a1d97bb1cac3fdbd6a35aa3dcbc285d258ae0e20
Vikram Singh <vs125609@gmail.com>
2023-01-15 00:53:45 +0530

    few api created

---
+++ backend/controllers/userController.js
@@ -0,0 +1,35 @@
+const { sendSuccess } = require("../utils/apiResponse");
+const catchAsync = require("../utils/catchAsync");
+const Follow = require('../models/followModel');
+const { default: mongoose } = require("mongoose");
+
+const followUser = catchAsync(async (req, res, next) => {
+    const id = req.params.id;
+    const user_id = "63c2e7131c650bfd601e000d";
+    const data = await Follow.find({ follower_id: id, user_id: user_id });
+    if (data.length == 0) {
+        const follow = new Follow({
+            user_id: user_id,
+            follower_id: id
+        })
+        await follow.save();
+    }
+    else {
+        await Follow.findOneAndDelete({ follower_id: id, user_id: user_id });
+    }
+    return sendSuccess(res, 200, "follower", data)
+});
+
+const allFollower = catchAsync(async (req, res, next) => {
+    const id = "63c2bc3b41ddb4301ac0a250";
+    const data = await Follow.find({ follower_id: id }).populate('user_id', 'first_name last_name image');
+    const finalResponse = data.map(x => {
+        return ({
+            first_name: x.user_id.first_name,
+            last_name: x.user_id.last_name,
+            image: x.user_id.image
+        })
+    })
+    return sendSuccess(res, 200, "all followers", finalResponse);
+})
+module.exports = { followUser, allFollower };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,3 +1,6 @@
 const express = require('express');
+const { followUser, allFollower } = require('../controllers/userController');
 const router = express.Router();
+router.post("/follow/:id", followUser);
+router.get("/allFollower", allFollower);
 module.exports = router;
