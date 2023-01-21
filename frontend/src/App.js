c66187aac04c2063d4c45a423197b243d0816984
Vikram Singh <vs125609@gmail.com>
2023-01-21 09:34:14 +0530

    loader animation on an api call is working fine but Spinner Component from the bootstrap is not working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import { allFollowers } from './Api/Api';
+import User from './Components/Users/User';
 const App = () => {
-  const [data, setData] = useState([]);
-  const allFollower = async () => {
-    const data = await allFollowers();
-    setData(data?.data?.data);
-  }
-  useEffect(() => {
-    allFollower();
-  },[])
   return (
     <>
-      {
-        data.map((data, index) => {
-          return (
-            <h1 key={index}>{data.first_name}</h1>
-          );
-        })
-      }
+      <User />
     </>
   );
 }
