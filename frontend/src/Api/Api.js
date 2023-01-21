51e6f437222c919a43300beb92efd820867c3b73
Vikram Singh <vs125609@gmail.com>
2023-01-21 09:49:57 +0530

    now the api loader is perfectly lies in the center of the webpage

--- frontend/src/Api/Api.js
+++ frontend/src/Api/Api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const API = axios.create({
-    baseURL: "http://localhost:3000/api/user/v1"
+    baseURL: "http://43.204.142.84/api/user/v1"
 })
 export const allFollowers = () => API.get("/allFollower");
