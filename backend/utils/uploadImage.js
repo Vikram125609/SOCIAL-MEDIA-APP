e21f44cb00bf2d8aef268a75b0348a34ceaedb39
Vikram Singh <vs125609@gmail.com>
2023-01-30 12:55:51 +0530

    image uploaded and stored in atlas

--- backend/utils/uploadImage.js
+++ backend/utils/uploadImage.js
@@ -16,8 +16,7 @@ const uploadImage = async (imagePath) => {
     };
     try {
         const result = await cloudinary.uploader.upload(imagePath, options);
-        console.log(result);
-        return result.public_id;
+        return result;
     } catch (error) {
         console.error(error);
     }
