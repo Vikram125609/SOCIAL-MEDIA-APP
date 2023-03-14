1a0bf6299ab2c2fe2863509f4b34be1bc44b8714
Vikram Singh <vs125609@gmail.com>
2023-03-14 14:51:49 +0530

    added plant.txt file

--- frontend/src/Components/Profile/Users.jsx
+++ frontend/src/Components/Profile/Users.jsx
@@ -1,10 +1,8 @@
-import { Link, useNavigate } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 import React from "react";
 import { Avatar, Button, Stack, Box, Container, Typography } from '@mui/material';
 const color = 'secondary';
 const Users = (props) => {
-    const navigate = useNavigate();
     const imageClicked = (e) => {
         window.open(e.target.src);
     }
@@ -18,14 +16,14 @@ const Users = (props) => {
                 {
                     data.map((data) => {
                         return (
-                            <Box onClick={() => {
-                                navigate(`/profile/${data._id}`, { replace: true })
-                            }} sx={{ display: 'flex', padding: '10px', margin: '10px 0px 10px 0px', borderRadius: '10px', justifyContent: 'space-between', alignItems: 'center', boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.43)' }} key={data._id}>
-                                <Avatar onClick={imageClicked} sx={{ height: 100, width: 100, border: '1px solid' }} src={data.image} />
-                                <Typography color='secondary' variant='body1'>{data.first_name}</Typography>
-                                <Typography color='secondary' variant='body1'>{data.last_name}</Typography>
-                                <Button color={color} variant='outlined'> Message </Button>
-                            </Box>
+                            <Link style={{ textDecoration: 'none' }} to={"/profile/" + data._id} >
+                                <Box sx={{ display: 'flex', padding: '10px', margin: '10px 0px 10px 0px', borderRadius: '10px', justifyContent: 'space-between', alignItems: 'center', boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.43)' }} key={data._id}>
+                                    <Avatar onClick={imageClicked} sx={{ height: 100, width: 100, border: '1px solid black' }} src={data.image} />
+                                    <Typography color='secondary' variant='body1'>{data.first_name}</Typography>
+                                    <Typography color='secondary' variant='body1'>{data.last_name}</Typography>
+                                    <Button color={color} variant='outlined'> Message </Button>
+                                </Box>
+                            </Link>
                         )
                     })
                 }
